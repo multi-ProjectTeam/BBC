cb594ac2ae014c526e7b8fae32f168753981dee3
Hangyeol-Jang <8524hg@gmail.com>
2021-10-15 11:13:30 +0900

    update initial files

--- bbc/src/main/java/com/example/bbc/BbcApplication.java
+++ bbc/src/main/java/com/example/bbc/BbcApplication.java
@@ -1,9 +1,11 @@
 package com.example.bbc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value= {"com.example.bbc.mapper"})
 public class BbcApplication {
 
 	public static void main(String[] args) {
